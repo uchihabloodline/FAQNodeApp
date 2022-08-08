9dd123c8f1e352084db2cf8f860d943f11afc78e
Shivam Pandey <shivam.pandey@payu.in>
2022-08-08 21:49:08 +0530

    removed logs

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -54,7 +54,6 @@ passport.checkAuthentication = function(req, res, next){
 passport.setAuthenticatedUser = function(req, res, next){
     if (req.isAuthenticated()){
         // req.user contains the current signed in user from the session cookie and we are just sending this to the locals for the views
-        console.log('pass- ', req.user);
         res.locals.user = req.user;
     }
 
