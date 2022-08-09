aa25f03881936d0ee20a60f0832d3c9843ae0752
Shivam Pandey <shivam.pandey@payu.in>
2022-08-09 12:37:45 +0530

    username and password made required

--- models/user.js
+++ models/user.js
@@ -3,12 +3,12 @@ const mongoose  = require('mongoose');
 const userSchema = new mongoose.Schema({
     email:{
         type: String,
-        //required:  true,
+        require: true,
         unique:    true,
     },
     password:{
         type:  String,
-        //required:  true,
+        require: true
     }
 },{
     timestamps: true
